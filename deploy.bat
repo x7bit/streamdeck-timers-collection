7d377deb36d3fee9fde67d60404b60010f13afc7
Rayco Alonso de la Rosa <raycoalonsodelarosa@gmail.com>
2022-07-06 21:31:36 +0100

    Removed deploy script

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SET BUILD_DIR="release"
-SET BUILD_FILE="%BUILD_DIR%\com.x7bit.precisetimer.streamDeckPlugin"
-SET SOURCE_DIR="src\com.x7bit.precisetimer.sdPlugin"
-IF EXIST %BUILD_FILE% DEL /F %BUILD_FILE%
-.\DistributionTool.exe -b -i %SOURCE_DIR% -o %BUILD_DIR%
